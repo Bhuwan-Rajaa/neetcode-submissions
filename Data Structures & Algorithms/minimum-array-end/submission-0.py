5ba6912ef1fcab1ad48ec2e1853435feffe1d5a9
C Bhuwan Rajaa <bhuwanrajaa21@gmail.com>
2026-08-18 11:56:13 +0530

    Add: minimum-array-end - submission-0

---
+++ Data Structures & Algorithms/minimum-array-end/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minEnd(self, n: int, x: int) -> int:
+        diff = 1
+        for i in range(1,32):
+            if x <= diff:
+                break
+            diff *= 2
+
+
+
+        
+        return x+diff*(n-1)
